409e362a5d87b3870280c323bdbce0a263cb6054
Simon Rupp <simonwrupp@gmail.com>
2024-01-04 01:11:40 -0500

    update

--- app/src/vite-env.d.ts
+++ app/src/vite-env.d.ts
@@ -1,5 +1,10 @@
 /// <reference types="vite/client" />
   
+    interface ImportMetaEnv {
+        VITE_API_URL: string
+        NODE_ENV: 'development' | 'production' | 'test'
+    }
+
   interface ImportMeta {
     readonly env: ImportMetaEnv
   }
