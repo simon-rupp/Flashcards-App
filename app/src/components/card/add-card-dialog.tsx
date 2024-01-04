3ee2fd0e295b3dce29ec4b94c86d41f86f657aac
Simon Rupp <simonwrupp@gmail.com>
2024-01-03 22:20:11 -0500

    update

--- app/src/components/card/add-card-dialog.tsx
+++ app/src/components/card/add-card-dialog.tsx
@@ -11,8 +11,6 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { PlusCircledIcon } from "@radix-ui/react-icons";
 import { useStore } from "@/lib/store";
 import useMutationcards from "@/hooks/use-mutations-cards";
